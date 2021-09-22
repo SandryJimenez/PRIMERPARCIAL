c7ddfafa7a4abc0b1ef38398f8dcf4b45b34bb07
SandryJimenez <90071133+SandryJimenez@users.noreply.github.com>
2021-09-22 10:29:08 -0500

    PRIMERPARCIAL
    SANDRY JIMENEZ MORALES

---
+++ Parcial_1/src/RedComputadores/Interfaz.java
@@ -0,0 +1,28 @@
+
+package RedComputadores;
+
+/**
+ *
+ * @author SANDRY
+ */
+public class Interfaz {
+    Red red = nwe Red ("UPC", 'A');
+    
+    Host hostl = new Host ("PCI", "Windows");
+    hostl.configurar(192,168,0,1);
+    
+    Host host2 = new Host ("PCI", "Windows");
+    hostl.configurar(192,168,0,1);
+    
+    
+    Host host3 = new Host ("PCI", "Windows");
+    hostl.configurar(192,168,0,1);
+    
+    Host host4 = new Host ("PCI", "Windows");
+    hostl.configurar(192,168,0,1);
+    
+
+    
+    
+    
+}
